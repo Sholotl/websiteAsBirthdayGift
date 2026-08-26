24d6a476c4fab2c627d71c18121d1090752d2a02
Sholotl <yolotl.teutli@gmail.com>
2026-08-26 00:05:10 -0600

    Dependencies are added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const map = L.map('map').setView([19.062556, -98.306361], 13)
+const map = L.map('map').setView([19.062556, -98.306361], 100)
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map)
+
+L.marker([19.062556, -98.306361]).addTo(map).bindPopup('Chama mi amol').openPopup();
