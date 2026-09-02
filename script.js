c5992fee0774852e04460c4098712fce0817b3e1
Sholotl <yolotl.teutli@gmail.com>
2026-09-02 00:51:25 -0600

    marker builder and main structure

--- script.js
+++ script.js
@@ -7,7 +7,51 @@ const Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/ali
 	ext: 'png'
 }).addTo(map);
 
-L.marker([19.0485386, -98.2166069])
-	.addTo(map)
-	.bindPopup('Chama mi amol')
-	.openPopup();
+function createMarkers(locations, content) 
+{
+    for (let location of locations) 
+		{
+        let info = content.find(item => item.id === location.id);
+
+        let popup = `
+            <h2>${info.titulo}</h2>
+            <p>${info.nota}</p>
+            <img src="${info.foto}" alt="${info.titulo}">
+        `;
+
+        if (info.audio !== null) {
+            popup += `
+                <audio controls>
+                    <source src="${info.audio}" type="audio/mpeg">
+                    Tu navegador no soporta audio.
+                </audio>
+            `;
+        }
+
+        L.marker([location.lat, location.lng])
+            .addTo(map)
+            .bindPopup(popup);
+    }
+}
+
+function loadLocations()
+{
+	return fetch("src/data/locations.json")
+        .then(response => response.json());
+}
+
+function loadContent()
+{
+	return fetch("src/data/content.json")
+        .then(response => response.json());
+}
+
+Promise.all([
+
+		loadLocations(),
+		loadContent()
+
+	]).then(([locations, content]) => {
+
+		createMarkers(locations, content)
+});
