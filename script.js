b62581f1ec930c118e15218e135649981c530a3e
Sholotl <yolotl.teutli@gmail.com>
2026-09-01 23:10:56 -0600

    code dependencies and tile provider

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const map = L.map('map').setView([19.062556, -98.306361], 100)
+const map = L.map('map').setView([19.0485386, -98.2166069], 15)
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map)
+const Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+}).addTo(map);
 
-L.marker([19.062556, -98.306361]).addTo(map).bindPopup('Chama mi amol').openPopup();
+L.marker([19.0485386, -98.2166069])
+	.addTo(map)
+	.bindPopup('Chama mi amol')
+	.openPopup();
